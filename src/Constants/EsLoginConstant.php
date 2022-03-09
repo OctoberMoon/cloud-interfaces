b30ad8f0d60cb1c5716e1e50d6332c4cca513ff9
雾霭 <15516319695@163.com>
2022-03-09 19:22:13 +0800

    test:1

--- src/Constants/EsLoginConstant.php
+++ src/Constants/EsLoginConstant.php
@@ -5,10 +5,12 @@ namespace OctoberMoon\CloudInterfaces\Constants;
 
 class EsLoginConstant
 {
-    public const LOGIN_TYPE_ECO                         = 1;
+    public const LOGIN_TYPE_ECO      = 1;
+    public const LOGIN_TYPE_ECO_SYNC = 2;
 
     public static array $ecoTurnChainLimitMap = [
-        self::LOGIN_TYPE_ECO     => 10,
+        self::LOGIN_TYPE_ECO      => 10,
+        self::LOGIN_TYPE_ECO_SYNC => 10,
     ];
-    
+
 }
